e3317a094b817fb455b3dd85d7c31a761ec0fe3a
Thomas Wilburn <news@thomaswilburn.net>
2016-01-24 15:05:12 -0800

    Add coloring to the current carbon score
    Fixes #5

--- src/js/sparkline.js
+++ src/js/sparkline.js
@@ -7,16 +7,14 @@ module.exports = function() {
     link: function(scope, element, attrs) {
       element = element[0];
       var canvas = element.querySelector("canvas");
-      var bounds = element.getBoundingClientRect();
-      canvas.width = bounds.width;
-      canvas.height = bounds.height;
       var context = canvas.getContext("2d");
       scope.$watch(function() {
         var max = Math.max.apply(null, scope.data);
         var min = Math.min.apply(null, scope.data.filter(n => n));
         var range = max - min;
         if (range < 1) range = 1;
-        context.clearRect(0, 0, canvas.width, canvas.height);
+        canvas.width = canvas.offsetWidth;
+        canvas.height = canvas.offsetHeight;
         context.beginPath();
         var start = canvas.height - (scope.data[scope.data.length - 1] - min) / range * canvas.height
         context.moveTo(0, start);
